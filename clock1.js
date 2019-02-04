2a11581855364fcf07af65629e65e21d999b5d71
TyDSmith <36959531+TyDSmith@users.noreply.github.com>
2019-02-03 23:43:47 -0800

    adjust for dynamic height and width variables

--- clock1.js
+++ clock1.js
@@ -1,10 +1,13 @@
 function setup() {
-  createCanvas(300, 300);
+	var canHeight = 300;
+	var canWidth = 300;
+  createCanvas(canWidth, canHeight);
 }
 
 function draw() {
   background(0);
-
+	var canHeight = 300;
+	var canWidth = 300;
 	let hr = hour();
 	let min = minute();
 	let sec = second();
@@ -12,7 +15,7 @@ function draw() {
 	strokeWeight(8);
 	stroke(255);
 	noFill();
-	ellipse(150,150,275,275);
+	ellipse((canHeight/2),canWidth/2,275,275);
 	
 	strokeWeight(6);
 	stroke(255,100,150);
